35b12e677e32b36fd43b0c47580caba3ae7cebf9
DisSys <20084833@mail.wit.ie>
2023-10-30 07:33:44 +0000

    CRUD

--- src/main/kotlin/org/setu/fantasyfootball/console/main/Main.kt
+++ src/main/kotlin/org/setu/fantasyfootball/console/main/Main.kt
@@ -1,8 +1,10 @@
 package org.setu.fantasyfootball.console.main
 
 import mu.KotlinLogging
+import org.setu.fantasyfootball.console.models.TeamModel
 
 private val logger = KotlinLogging.logger{}
+val teams = ArrayList<TeamModel>()
 
 fun main(args: Array<String>) {
     logger.info {"Launching Fantasy NFL App"}
@@ -13,9 +15,11 @@ fun main(args: Array<String>) {
     do{
         input = menu()
         when(input){
-            1 -> println("You Chose To Add Team")
-            2 -> println("You Chose To Update Teams")
-            3 -> println("You Chose To List All Teams")
+            1 -> addTeam()
+            2 -> updateTeam()
+            3 -> listTeams()
+            4 -> searchTeam()
+            -99 -> dummyData()
             -1 -> println("Exiting App")
             else -> println("Invalid Option Try Again")
         }
@@ -33,6 +37,7 @@ fun menu() : Int{
     println(" 1. Add Team")
     println(" 2. Update Team")
     println(" 3. List All Teams")
+    println(" 4. Search For A Team")
     println("-1. Exit")
     println()
     print("Enter an Integer : ")
@@ -42,4 +47,91 @@ fun menu() : Int{
     else
         -9
     return option
+}
+
+fun addTeam(){
+    var aTeam = TeamModel()
+    println("Add Team")
+    println()
+    print("Enter a Title: ")
+    aTeam.title = readln()!!
+    print("Enter a Description: ")
+    aTeam.description = readln()!!
+
+    if (aTeam.title.isNotEmpty() && aTeam.description.isNotEmpty()){
+        aTeam.id = teams.size.toLong()
+        teams.add(aTeam.copy())
+        logger.info("Team Added: [$aTeam]")
+    }
+    else
+        logger.info("Team Not Added")
+}
+
+fun updateTeam(){
+    println("Update Team")
+    println()
+    listTeams()
+    var searchId = getId()
+    val aTeam = search(searchId)
+    var tempTitle: String?
+    var tempDescription: String?
+
+    if(aTeam != null){
+        print("Enter A New Title For ["+ aTeam.title +"] : ")
+        tempTitle = readln()!!
+        print("Enter A New Description For ["+ aTeam.description +"] : ")
+        tempDescription = readln()!!
+
+        if(!tempTitle.isNullOrEmpty() && !tempDescription.isNullOrEmpty()){
+            aTeam.title = tempTitle
+            aTeam.description =tempDescription
+            println(
+                "You updated [ "+ aTeam.title +" ] for title "+
+                "and [ "+ aTeam.description +" ] for description")
+            logger.info("Team Updated: [ $aTeam ] ")
+        }
+        else
+            logger.info("Team Not Updated")
+    }
+    else println("Team Not Updated...")
+}
+
+fun listTeams(){
+    println("List of All Teams")
+    println()
+    teams.forEach{ logger.info("${it}") }
+    println()
+}
+
+fun searchTeam(){
+    var searchId = getId()
+    val aTeam = search(searchId)
+
+    if(aTeam != null)
+        println("Team Details [ $aTeam ] ")
+    else
+        println("Team Not Found Try Again...")
+}
+
+fun getId(): Long{
+    var strId : String?
+    var searchId : Long?
+    print("Enter ID to Search/Update: ")
+    strId = readln()!!
+    searchId = if(strId.toLongOrNull() != null && !strId.isEmpty())
+        strId.toLong()
+    else
+        -9
+    return searchId
+}
+
+fun search(id: Long) : TeamModel? {
+    var foundTeam : TeamModel? = teams.find { p -> p.id == id }
+    return foundTeam
+}
+
+fun dummyData(){
+    teams.add(TeamModel(1, "Stealers", "Favorite team"))
+    teams.add(TeamModel(2, "New York Giants", "Some big lads on that team"))
+    teams.add(TeamModel(3, "The Jets", "Them lads can run"))
 }
---
+++ src/main/kotlin/org/setu/fantasyfootball/console/models/TeamModel.kt
@@ -0,0 +1,7 @@
+package org.setu.fantasyfootball.console.models
+
+import jdk.jfr.Description
+
+data class TeamModel (var id: Long = 0,
+                      var title: String = "",
+                      var description: String = "")
